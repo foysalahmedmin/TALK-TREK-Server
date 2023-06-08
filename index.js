b630516c404b7ff26794c3bdf5e2060d2674aeef
FOYSAL AHMED MiN <foysalahmedmin@gmail.com>
2023-06-08 17:06:31 +0600

    set jwt token system and add user functionality

--- index.js
+++ index.js
@@ -2,14 +2,30 @@ const express = require('express')
 const app = express()
 const cors = require('cors');
 require('dotenv').config()
+const jwt = require('jsonwebtoken');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const port = process.env.PORT || 5000
 
 app.use(cors())
 app.use(express.json())
 
+const verifyJWT = (req, res, next) => {
+    const authorization = req.headers.authorization;
+    if (!authorization) {
+        return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
 
+    const token = authorization.split(' ')[1];
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ error: true, message: 'unauthorized access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 const uri = `mongodb+srv://${process.env.DB_USER_NAME}:${process.env.DB_PASSWORD}@cluster0.7y3daag.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -24,7 +40,30 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // await client.connect();
+        const database = client.db("TalkTrekDB");
+        const UserCollection = database.collection("UserCollection");
 
+        //JWT
+        app.post('/jwt', (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+            res.send({ token })
+        })
+
+        //User
+        app.post('/user', async(req, res) => {
+            const user = req.body
+            const query = {
+                email: user.email
+            }
+            const userExist = await UserCollection.findOne(query) ;
+            if (userExist){
+                return
+            }else{
+                const result = await UserCollection.insertOne(user);
+                res.send(result);
+            }
+        })
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
